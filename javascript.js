a802d880f006c8d771e836941af73e98d733fb7f
mercy347 <MercyMwendeKitheka@gmail.com>
2023-06-13 09:49:21 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+
+    function calculate(num1, num2, operator) {
+
+        if (operator === '+') {
+            console.log(num1 + num2)
+        }
+
+
+    }
+    calculate(1,2 ,'+')
+
+    function calculate(num1, num2,operator) {
+
+    if(operator === "-") {
+        console.log(num1 - num2)
+
+    }
+
+}
+
+    calculate(1,2,"-")
+
+    function calculate(num1,num2,operator) {
+
+        if (operator === "/")
+        console.log(num1 / num2)
+
+    }
+
+    calculate(1,2,"/")
+
+    function calculate(num1,num2,operator) {
+
+    if(operator === "*")
+    console.log(num1 * num2)
+    }
+
+    calculate(1,2,"*")
